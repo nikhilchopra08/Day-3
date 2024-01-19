5c0c5c2ae29830b5e6b24708796e7a21ba5f10dd
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2024-01-19 12:20:24 +0530

    Palindrome

--- Strings.cpp
+++ Strings.cpp
@@ -21,6 +21,21 @@ void Reverse(char name[] , int n){
     }
 }
 
+bool checkPalindrome(char a[] , int n){
+    int s = 0;
+    int e = n-1;
+
+    while(s < e){
+        if(a[s] != a[e]){
+            return 0;
+        }else{
+            s++;
+            e--;
+        }
+        return 1;
+    }
+}
+
 int main(){
     char name[20];
 
@@ -36,5 +51,11 @@ int main(){
 
     Reverse(name , length);
     cout<< "name ; " << name << endl;
+
+    bool palindrome = checkPalindrome(name , length);
+
+    cout << "the string is " << palindrome;
+
+    
     return 0;
 }
