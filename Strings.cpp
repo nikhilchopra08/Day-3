67c930be118f35a05b036aa989474a5b4b47d2e6
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2024-01-19 12:01:00 +0530

    Length of String

--- Strings.cpp
+++ Strings.cpp
@@ -2,15 +2,26 @@
 
 using namespace std;
 
+int GetLength(char name[]){
+    int count = 0;
+
+    for(int i = 0; name[i] != '\0' ; i++){
+        count++;
+    }
+
+    return count;
+}
+
 int main(){
     char name[20];
 
     cout << "Enter your name" << endl;
     cin >> name;
-    name[2] = '\0';
+    // name[2] = '\0';
     //cin will not take the entry after space.space denotes null char \0;;
 
     cout << "your name is " << name << endl;
+    cout << "Length of Char Array is " << GetLength(name) << endl;
 
     return 0;
 }
